d1252e6f9d67806761ce624f9e67f26282f3b284
ouhadjilyes <ouhadjilyes@protonmail.com>
2022-12-30 03:13:25 -0800

    added kameleo and stealth package

---
+++ kameleo_impl.py
@@ -0,0 +1,44 @@
+from kameleo.local_api_client.kameleo_local_api_client import KameleoLocalApiClient
+from kameleo.local_api_client.builder_for_create_profile import BuilderForCreateProfile
+from kameleo.local_api_client.models.problem_response_py3 import ProblemResponseException
+from playwright.sync_api import sync_playwright
+import json
+
+
+try:
+    # This is the port Kameleo.CLI is listening on. Default value is 5050, but can be overridden in appsettings.json file
+    kameleo_port = 5050
+
+    client = KameleoLocalApiClient(f'http://localhost:{kameleo_port}')
+
+    # Search Chrome Base Profiles
+    base_profiles = client.search_base_profiles(
+        device_type='desktop',
+        browser_product='chrome'
+    )
+
+    # Create a new profile with recommended settings
+    # Choose one of the Base Profiles
+    create_profile_request = BuilderForCreateProfile \
+        .for_base_profile(base_profiles[0].id) \
+        .set_recommended_defaults() \
+        .build()
+    profile = client.create_profile(body=create_profile_request)
+
+    # Start the browser profile
+    client.start_profile(profile.id)
+
+    # Connect to the browser with Playwright through CDP
+    browser_ws_endpoint = f'ws://localhost:{kameleo_port}/playwright/{profile.id}'
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.connect_over_cdp(endpoint_url=browser_ws_endpoint)
+        context = browser.contexts[0]
+        page = context.new_page()
+
+        # Use any Playwright command to drive the browser
+        page.goto('https://google.com')
+
+    # Stop the browser by stopping the Kameleo profile
+    client.stop_profile(profile.id)
+except ProblemResponseException as e:
+    raise Exception([str(e), json.dumps(e.error.error)])
---
+++ stealth_package.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+#
+#
+# pip install playwright-stealth
+#
+#
+
+
+import asyncio
+from playwright.async_api import async_playwright
+from playwright_stealth import stealth_async
+
+async def main():
+    async with async_playwright() as p:
+        for browser_type in [p.chromium, p.firefox, p.webkit]:
+            browser = await browser_type.launch()
+            page = await browser.new_page()
+            await stealth_async(page)
+            await page.goto('http://whatsmyuseragent.org/')
+            await page.screenshot(path=f'example-{browser_type.name}.png')
+            await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
